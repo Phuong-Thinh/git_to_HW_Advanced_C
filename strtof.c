00a4a15396fa4ab3cc07b7ef10319d36cc0a7b88
Thinh <phan.phuong.thinh.21.02@gmail.com>
2022-07-23 11:47:19 +0700

    update strtof.c file

--- strtof.c
+++ strtof.c
@@ -3,12 +3,12 @@
 
 
 
-double test_atof(char *string){
+double test_atof(char *string){ // tham khảo của Trinh Trinh.
     double result = 0, count = 0;
     for (int i = 0; string[i] != '\0'; i++){
         if (string[i] == '.'){
             count++;
-            continue;
+            continue; // bỏ qua câu lên liền kề (if else) rồi count thanh 1 ung với 1/10 và if đúng
         }
         if (count){
             count *= 10;
@@ -20,15 +20,15 @@ double test_atof(char *string){
 }
 
 int main(){
-    // /* TEST*/
-    // printf("\n Test");
-    // char *pt=NULL;
-    // char arr[]="70.215 0.7";
-    // char **pt2;
-    // float f1 = strtof(arr,&pt );
-    // float f2 = strtof(pt, NULL);
-    // printf("\n %.3f", f1);
-    // printf("\n %.1f \n -------------------------------------", f2);
+    /* TEST*/
+    printf("\n Test");
+    char *pt=NULL;
+    char arr[]="70.215 0.7";
+    char **pt2;
+    float f1 = strtof(arr,&pt );
+    float f2 = strtof(pt, NULL);
+    printf("\n %.3f", f1);
+    printf("\n %.1f \n -------------------------------------", f2);
 
     // /*------------*/
     char  *str ="70.215 0.7";
